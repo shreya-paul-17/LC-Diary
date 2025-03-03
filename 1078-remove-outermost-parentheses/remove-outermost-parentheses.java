c6c01e7a6d2789d0e23d160feab06b6211dc91a1
Shreya Paul <135226069+shreya-paul-17@users.noreply.github.com>
2025-03-03 20:08:03 +0530

    Time: 2 ms (99.08%) | Memory: 42.2 MB (78.59%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder str = new StringBuilder();
+        int count = 0;
+        for (char ch :  s.toCharArray()) {
+            if (ch == '(') {
+                if (count > 0) str.append("(");
+                count++;
+            } else {
+                count--;
+                if (count > 0) str.append(")");
+            }
+        }
+        return str.toString();
+    }
+}
