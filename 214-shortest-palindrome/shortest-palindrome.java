3961ebd0ba0c500a6b8e113fd004a4dae606a1d8
Shreya Paul <135226069+shreya-paul-17@users.noreply.github.com>
2025-03-08 13:07:32 +0530

    Time: 3 ms (97.05%) | Memory: 44.5 MB (99.40%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.java
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -1,4 +1,5 @@
 // 1. Brute force approach
+// TC: O(N^2) ; SC: O(N)
 
 // class Solution {
 //     public String shortestPalindrome(String s) {
@@ -15,6 +16,7 @@
 // }
 
 // 2. KMP (Knuth-Morris-Pratt) Algorithm
+// TC: O(N); SC: O(N)
 
 // class Solution {
 //     public String shortestPalindrome(String s) {
@@ -40,58 +42,60 @@
 // }
 
 // 3. Rabin-Karp (Rolling Hash)
-
-// class Solution {
-//     public String shortestPalindrome(String s) {
-//         int n = s.length();
-//         int P = 31, MOD = 1000000007;
-//         long POW = 1, h1 = 0, h2 = 0;
-//         int maxPalindromicPrefixLength = 0;
-
-//         for (int i = 0; i < n; i++) {
-//             int charInt = s.charAt(i) - 'a' + 1;
-//             h1 = (h1 * P + charInt) % MOD;
-//             h2 = (charInt * POW + h2) % MOD;
-//             if (h1 == h2) {
-//                 maxPalindromicPrefixLength = i + 1;
-//             }
-//             POW = (POW * P) % MOD;
-//         }
-
-//         return new StringBuilder(s.substring(maxPalindromicPrefixLength)).reverse().toString() + s;
-//     }
-// }
-
-// 4. Z-Algorithm
+// TC:O(N); SC:O(N)
 
 class Solution {
     public String shortestPalindrome(String s) {
-        String rs = new StringBuilder(s).reverse().toString();
-        String ts = s + "#" + rs;
-        int n = ts.length();
-        int[] z = new int[n];
-        int l = 0, r = 0, maxLen = 0;
-
-        for (int i = 1; i < n; i++) {
-            if (i <= r) {
-                z[i] = Math.min(r - i + 1, z[i - l]);
-            }
-            while (i + z[i] < n && ts.charAt(z[i]) == ts.charAt(i + z[i])) {
-                z[i]++;
-            }
-            if (i + z[i] - 1 > r) {
-                l = i;
-                r = i + z[i] - 1;
-            }
-        }
+        int n = s.length();
+        int P = 31, MOD = 1000000007;
+        long POW = 1, h1 = 0, h2 = 0;
+        int maxPalindromicPrefixLength = 0;
 
         for (int i = 0; i < n; i++) {
-            if (z[i] == n - i) {
-                maxLen = z[i];
-                break;
+            int charInt = s.charAt(i) - 'a' + 1;
+            h1 = (h1 * P + charInt) % MOD;
+            h2 = (charInt * POW + h2) % MOD;
+            if (h1 == h2) {
+                maxPalindromicPrefixLength = i + 1;
             }
+            POW = (POW * P) % MOD;
         }
 
-        return new StringBuilder(s.substring(maxLen)).reverse().toString() + s;
+        return new StringBuilder(s.substring(maxPalindromicPrefixLength)).reverse().toString() + s;
     }
 }
+
+// 4. Z-Algorithm
+// TC: O(N); SC: O(N)
+
+// class Solution {
+//     public String shortestPalindrome(String s) {
+//         String rs = new StringBuilder(s).reverse().toString();
+//         String ts = s + "#" + rs;
+//         int n = ts.length();
+//         int[] z = new int[n];
+//         int l = 0, r = 0, maxLen = 0;
+
+//         for (int i = 1; i < n; i++) {
+//             if (i <= r) {
+//                 z[i] = Math.min(r - i + 1, z[i - l]);
+//             }
+//             while (i + z[i] < n && ts.charAt(z[i]) == ts.charAt(i + z[i])) {
+//                 z[i]++;
+//             }
+//             if (i + z[i] - 1 > r) {
+//                 l = i;
+//                 r = i + z[i] - 1;
+//             }
+//         }
+
+//         for (int i = 0; i < n; i++) {
+//             if (z[i] == n - i) {
+//                 maxLen = z[i];
+//                 break;
+//             }
+//         }
+
+//         return new StringBuilder(s.substring(maxLen)).reverse().toString() + s;
+//     }
+// }
