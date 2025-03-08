28934fade2d6256c71e4bd71e2fceb89dfc404ee
Shreya Paul <135226069+shreya-paul-17@users.noreply.github.com>
2025-03-08 13:02:41 +0530

    Time: 7 ms (85.46%) | Memory: 45 MB (87.52%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.java
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -13,26 +13,48 @@
 //     }
 // }
 
-// KMP Algorithm
+// KMP (Knuth-Morris-Pratt) Algorithm
+// class Solution {
+//     public String shortestPalindrome(String s) {
+//         String r = new StringBuilder(s).reverse().toString();
+//         String ts = s + "#" + r;
+//         int n = ts.length();
+//         int[] pi = new int[n];
+
+//         // Compute prefix function (KMP partial match table)
+//         for (int i = 1; i < n; i++) {
+//             int j = pi[i - 1];
+//             while (j > 0 && ts.charAt(i) != ts.charAt(j)) {
+//                 j = pi[j - 1];
+//             }
+//             if (ts.charAt(i) == ts.charAt(j)) {
+//                 j++;
+//             }
+//             pi[i] = j;
+//         }
+
+//         return r.substring(0, r.length() - pi[n - 1]) + s;
+//     }
+// }
+
+// Rabin-Karp (Rolling Hash)
 class Solution {
     public String shortestPalindrome(String s) {
-        String r = new StringBuilder(s).reverse().toString();
-        String ts = s + "#" + r;
-        int n = ts.length();
-        int[] pi = new int[n];
+        int n = s.length();
+        int P = 31, MOD = 1000000007;
+        long POW = 1, h1 = 0, h2 = 0;
+        int maxPalindromicPrefixLength = 0;
 
-        // Compute prefix function (KMP partial match table)
-        for (int i = 1; i < n; i++) {
-            int j = pi[i - 1];
-            while (j > 0 && ts.charAt(i) != ts.charAt(j)) {
-                j = pi[j - 1];
+        for (int i = 0; i < n; i++) {
+            int charInt = s.charAt(i) - 'a' + 1;
+            h1 = (h1 * P + charInt) % MOD;
+            h2 = (charInt * POW + h2) % MOD;
+            if (h1 == h2) {
+                maxPalindromicPrefixLength = i + 1;
             }
-            if (ts.charAt(i) == ts.charAt(j)) {
-                j++;
-            }
-            pi[i] = j;
+            POW = (POW * P) % MOD;
         }
 
-        return r.substring(0, r.length() - pi[n - 1]) + s;
+        return new StringBuilder(s.substring(maxPalindromicPrefixLength)).reverse().toString() + s;
     }
 }
